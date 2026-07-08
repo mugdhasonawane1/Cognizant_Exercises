1b8c8ba6eaef28232c6936ee2bdf73632150c2d0
mugdhasonawane1 <mugdhasonawane2005@gmail.com>
2026-07-08 11:30:57 +0530

    Completed EF Core Lab 4

--- RetailInventory/Program.cs
+++ RetailInventory/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
 
-namespace RetailInventory
+using var context = new AppDbContext();
+
+var electronics = new Category
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Retail Inventory System");
-        }
-    }
-}
+    CategoryName = "Electronics"
+};
+
+var groceries = new Category
+{
+    CategoryName = "Groceries"
+};
+
+await context.Categories.AddRangeAsync(electronics, groceries);
+
+var product1 = new Product
+{
+    ProductName = "Laptop",
+    Price = 75000,
+    StockQuantity = 10,
+    Category = electronics
+};
+
+var product2 = new Product
+{
+    ProductName = "Rice Bag",
+    Price = 1200,
+    StockQuantity = 50,
+    Category = groceries
+};
+
+await context.Products.AddRangeAsync(product1, product2);
+
+await context.SaveChangesAsync();
+
+Console.WriteLine("Data inserted successfully!");
